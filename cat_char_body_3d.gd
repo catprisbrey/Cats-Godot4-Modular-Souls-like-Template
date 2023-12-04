652f65584d21c43e2f234b80f9e05ad0aaa20151
Cat <43008564+catprisbrey@users.noreply.github.com>
2023-12-04 07:26:30 -0700

    Create cat_char_body_3d.gd
    init commit

---
+++ cat_char_body_3d.gd
@@ -0,0 +1,81 @@
+extends CharacterBody3D
+
+@onready var current_camera = get_viewport().get_camera_3d()
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var input_dir : Vector2
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+
+func _physics_process(_delta):
+	if !is_on_floor():
+		velocity.y -= gravity * _delta
+	move_player()
+	rotate_player()
+	
+func move_player():
+	# Get the movement orientation from the angles of the player to the camera.
+	# Using only camera's basis rotation created weird speed inconsistencies at downward angles
+	var new_direction = calc_direction()
+	if new_direction:
+		var rate : float # imiates directional change acceleration rate
+		if is_on_floor():
+			rate = .5
+		else:
+			rate = .1 # Makes it hard to change directions once in midair
+		velocity.x = move_toward(velocity.x, new_direction.x * SPEED, rate)
+		velocity.z = move_toward(velocity.z, new_direction.z * SPEED, rate)
+	else: # Smoothly come to a stop
+		velocity.x = move_toward(velocity.x, 0, .5)
+		velocity.z = move_toward(velocity.z, 0, .5)
+
+	move_and_slide()
+	
+func rotate_player():
+	var strafing = false
+	var dodging = false
+	var target_rotation
+	var current_rotation = global_transform.basis.get_rotation_quaternion()
+	
+	# StrafeCam code - Look at target, slerping current rotation to the camera's rotation.
+	if strafing == true && dodging == false: # Strafing looks at enemy
+		target_rotation = current_rotation.slerp(Quaternion(Vector3.UP, current_camera.global_rotation.y + PI), 0.4)
+		global_transform.basis = Basis(target_rotation)
+	
+	# Otherwise freelook, which is when not strafing or dodging, as well as, when rolling as you strafe. 
+	elif (strafing == false and dodging == false) or (strafing == true && dodging == true): # .... else:
+		
+		# FreeCam rotation code, slerps to input oriented to the camera perspective, and only calculates when input is given
+		if input_dir:
+			var new_direction = calc_direction().normalized()
+		# Rotate the player per the perspective of the camera
+			target_rotation = current_rotation.slerp(Quaternion(Vector3.UP, atan2(new_direction.x, new_direction.z)), 0.2)
+			global_transform.basis = Basis(target_rotation)
+	# move_and_slide() unused. (controlled by States).
+
+func calc_direction():
+	# calculate and return the direction of movement oriented to the current camera
+	var forward_vector = Vector3(0, 0, 1).rotated(Vector3.UP, current_camera.global_rotation.y)
+	var horizontal_vector = Vector3(1, 0, 0).rotated(Vector3.UP, current_camera.global_rotation.y)
+	var direction = (forward_vector * input_dir.y + horizontal_vector * input_dir.x)
+	return direction
+
+func _input(_event:InputEvent):
+	input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	calc_direction()
+	
+	if _event.is_action_pressed("ui_accept"):
+		jump()
+		
+	if _event.is_action_released("ui_left") \
+	or _event.is_action_released("ui_right") \
+	or _event.is_action_released("ui_up") \
+	or _event.is_action_released("ui_down"):
+		current_camera = get_viewport().get_camera_3d()
+
+func jump():
+	if is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
