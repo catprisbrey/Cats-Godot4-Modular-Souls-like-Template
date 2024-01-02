60567fda82a006a0e260d6b74083e27c5883ae2c
Cat <catprisbrey@gmail.com>
2024-01-02 13:51:44 -0700

    Heavy and Light animation trees made

--- player/AnimationTree.gd
+++ player/AnimationTree.gd
@@ -1,8 +1,22 @@
 extends AnimationTree
  
 @export var player_node : CharacterBody3D
-@onready var state_machine :AnimationNodeStateMachinePlayback= self["parameters/Light_tree/playback"]
-var movement = 0
+@onready var state_machine :AnimationNodeStateMachinePlayback= self["parameters/LIGHT_tree/playback"]
+
+var weapon_type = "LIGHT"
+
+func _input(event):
+	if event.is_action_pressed("ui_select"):
+		weaponchange()
+
+func weaponchange():
+	match weapon_type:
+		"LIGHT":
+			weapon_type = "HEAVY"
+		"HEAVY":
+			weapon_type = "LIGHT"
+	print(weapon_type)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#self["parameters/playback"].travel("Light_tree")
@@ -10,9 +24,16 @@ func _ready():
 	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	set_movement()
+	if player_node.strafing:
+		set_strafe()
+	else:
+		set_free_move()
 	
-func set_movement():
+func set_strafe():
 	print(player_node.strafe_cross_product.y)
-	set("parameters/Light_tree/MoveStrafe/blend_position", Vector2(player_node.strafe_cross_product.y,0))
-		
+	var new_blend = Vector2(player_node.strafe_cross_product.y,player_node.input_dir.y)
+	set("parameters/" + weapon_type + "_tree/MoveStrafe/blend_position", new_blend)
+
+func set_free_move():
+	var new_blend = Vector2(0,abs(player_node.input_dir.x) + abs(player_node.input_dir.y))
+	set("parameters/" + weapon_type + "_tree/MoveStrafe/blend_position",new_blend)
