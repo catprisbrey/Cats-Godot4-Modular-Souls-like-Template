2e6cbbb1f91c31d8e759e12548354260fbefe3ac
Cat <catprisbrey@gmail.com>
2024-02-25 12:07:51 -0700

    Fixed Interactable physics layer
    Accidentlly left a physics layer flag set while testing.

--- interactable objects/interactable_base.gd
+++ interactable objects/interactable_base.gd
@@ -3,7 +3,7 @@ class_name InteractableObject
 
 @export_enum("GENERIC","DOOR","GATE","CHEST","LEVER","LADDER") var interact_type : String = "GENERIC"
 @export var group_name :String = "Interactable"
-@export_flags_3d_physics var physical_layer = 9
+@export_flags_3d_physics var physical_layer = 8
 
 signal interactable_activated
 
